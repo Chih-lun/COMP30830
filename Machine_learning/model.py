b2830d14d1f3f02abb21cba271df3181c4c2d13c
tomsong2020 <92161905+tomsong2020@users.noreply.github.com>
2022-03-29 09:58:33 +0100

    ML second draft

---
+++ Machine_learning/model.py
@@ -0,0 +1,27 @@
+from pip import main
+from statsmodels.tsa.api import VAR
+from sqlalchemy import create_engine
+import pandas as pd
+
+URI = "database-1.c5ekejexdq4k.us-east-1.rds.amazonaws.com"
+PORT = "3306"
+DB = "dublin_bike"
+USER = "admin"
+PASSWARD = "group888"
+
+engine = create_engine("mysql+mysqlconnector://{}:{}@{}:{}/{}".format(USER, PASSWARD, URI, PORT, DB))
+df_bike = pd.read_sql_table("bike_availibility", engine, parse_dates="True", index_col=0)
+df_weather = pd.read_sql_table("weather", engine, parse_dates="True", index_col=0)
+
+df_combine = df_bike.merge(df_weather,left_on='Time', right_on='Time')
+
+def build_model(number,step):
+    # 5 min per step
+    df_test_station=df_combine.groupby("Number").get_group(number)
+    df_test_station = df_test_station.drop(['Number', 'ID_x', 'ID_y', 'Status', 'Weather', "Feels_like"], axis=1)
+    df_test_station = df_test_station.set_index('Time')
+    
+    model = VAR(df_test_station)
+    result = model.fit(4)
+    pred = result.forecast(y=df_test_station, steps=12) 
+    return pred
