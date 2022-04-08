fc53c8e4ae98a79efe99b19659b217e9aa5fb828
tomsong2020 <92161905+tomsong2020@users.noreply.github.com>
2022-04-08 09:49:44 +0100

    test

--- Machine_learning/model.py
+++ Machine_learning/model.py
@@ -1,7 +1,11 @@
+#  A simple py file that perform model building and output result
+
+# Import packages
 from statsmodels.tsa.api import VAR
 from sqlalchemy import create_engine
 import pandas as pd
 
+# DB info
 URI = "database-1.c5ekejexdq4k.us-east-1.rds.amazonaws.com"
 PORT = "3306"
 DB = "dublin_bike"
@@ -15,6 +19,7 @@ df_weather = pd.read_sql_table("weather", engine, parse_dates="True", index_col=
 df_combine = df_bike.merge(df_weather,left_on='Time', right_on='Time')
 
 def build_model(number,step):
+    """Take the station ID and step interval as input and return the prediction in a dataframe"""
     # 10 min per step
     df_test_station=df_combine.groupby("Number").get_group(number)
     df_test_station = df_test_station.drop(['Number', 'ID_x', 'ID_y', 'Status', 'Weather', "Feels_like"], axis=1)
@@ -22,10 +27,11 @@ def build_model(number,step):
     
     model = VAR(df_test_station)
     result = model.fit(4)
-    pred = result.forecast(y=df_test_station.values, steps=step)
+    pred = result.forecast(y=df_test_station.values[-4:], steps=step)
     df_pred = pd.DataFrame(pred, columns=["Aviable_bike_stands", "Available_bike", "Temp", "Humidity"])
     return df_pred
 
 
 # Simple test
-print(build_model(10, 2)["Temp"]) 
+print(build_model(10, 2)["Available_bike"]) 
+print(build_model(42, 2)["Available_bike"])
